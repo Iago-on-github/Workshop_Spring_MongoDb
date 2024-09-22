30ab21b7542629316adbd853c1611bbc7bc69cf1
Iago Araujo <Iago737@outlook.com>
2024-09-22 10:07:57 -0300

    Initial commit

---
+++ src/test/java/com/workshop/MongoDb/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.MongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
